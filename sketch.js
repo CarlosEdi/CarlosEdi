b40c5a813d60488cceb8ed58f649e461439a8a7e
CarlosEdi <105317911+CarlosEdi@users.noreply.github.com>
2022-05-10 18:44:38 -0300

    velocidade

--- sketch.js
+++ sketch.js
@@ -12,8 +12,8 @@ let xBolinha = 300;
 let yBolinha = 200 ;
 let diametro = 20;
 let raio= diametro/2;
-let velocidadexBolinha= 6;
-let velocidadeyBolinha = 6;
+let velocidadexBolinha= 20;
+let velocidadeyBolinha = 20;
 
 let hit = false; 
 
